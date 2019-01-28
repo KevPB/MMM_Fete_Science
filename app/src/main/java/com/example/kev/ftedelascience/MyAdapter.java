ad77f4f6cdc27580996f297cedc818987549ac07
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2019-01-28 16:19:01 +0100

    Adapteur

---
+++ app/src/main/java/com/example/kev/ftedelascience/MyAdapter.java
@@ -0,0 +1,23 @@
+package com.example.kev.ftedelascience;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder myViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
